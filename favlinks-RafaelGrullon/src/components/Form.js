1152efd63a5d860c6c3211092145cc7db1bcd7a2
grullonr <103902530+grullonr@users.noreply.github.com>
2023-04-28 22:51:45 -0400

    Update Form.js
    Updated the Form section of the Favlinks

--- favlinks-RafaelGrullon/src/components/Form.js
+++ favlinks-RafaelGrullon/src/components/Form.js
@@ -1,29 +1,69 @@
 import { useState } from 'react'
 
+class Form extends useState {
+  
+  render() {
+    const { handleSubmit, setName, userName, userLink, setLink } = this.props;
+    return (
+      
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        handleSubmit();
+      }}>
 
-//I got confused here
+        <div style={{ marginLeft: '200px' }}>
+         
+          <div style={{marginBottom:'10px' ,}}>
+          
+          <div style={{ fontSize: '15px' , fontWeight:500, marginBottom:'5px' }}> Name </div>
+            
+            <input
+              value={userName}
+              onChange={(event) => setName(event.target.value)}
+              type="name"
+              name="task"
+              style={{ width: '70%', padding: '8px',borderRadius:'4px' }}
+            />
 
-const Form = () => {
-  const handleChange = (event) => {
-    /*
-            TODO - Logic for changing state based on form changes
-        */
+          </div>
+
+          <div>
+            
+            <div style={{ fontSize: '15px' , fontWeight:400 , marginBottom:'5px'}}> URL </div>
+           
+            <input
+              value={userLink}
+              onChange={(event) => setLink(event.target.value)}
+              type="link"
+              name="link"
+              style={{ width: '70%', padding: '8px',borderRadius:'4px' }}
+            />
+
+          </div>
+
+          <div style={{ width: '50px', height: '50px', marginTop: '7px' }}>
+           
+            <button
+              type="submit"
+              style={{
+                color: 'white',
+                fontSize: '14px',
+                width: '50px',
+                height: '50px',
+                cursor:'pointer',
+                borderRadius:'4px'
+              }}
+            >
+
+              Submit
+
+            </button>
+
+          </div>
+
+        </div>
+
+      </form>
+    );
   }
-
-  const onFormSubmit = (event) => {
-    // to prevent page reload on form submit
-    event.preventDefault()
-
-    /*
-            TODO - Logic for calling props to handle submission and setting state changes
-        */
-  }
-
-  return (
-    <form>
-      {/* TODO - Logic for returning a form element with labels and inputs for link name and URL */}
-    </form>
-  )
 }
-
-export default Form
